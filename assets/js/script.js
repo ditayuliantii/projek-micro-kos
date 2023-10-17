210058800849b5f41e3994511c204b533a22a899
ditayuliantii <ydita97@gmail.com>
2023-10-18 06:38:42 +0700

    update file js fitur chat

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,6 @@ var swiper = new Swiper(".mySwiperPopuler", {
         nextEl: ".bx-chevron-right-circle",
         prevEl: ".bx-chevron-left-circle",
     },
-
 });
 
 
@@ -16,20 +15,28 @@ document.getElementById("lihatSemuaKos").addEventListener("click", function() {
 });
 
 
-document.getElementById("masukButton").addEventListener('click', function() {
+function login(){
     window.location.href = "masuk-login/login.html";
-});
+}
+
+function login2(){
+    window.location.href = "../masuk-login/login.html";
+}
+
+function toPembayaran(){
+    window.location.href = "pembayaran.html";
+}
 
 
-// document.getElementById("favorit").addEventListener('click', function() {
-//     window.location.href = "favorit/favorit.html";
+// document.getElementById("masukButton").addEventListener('click', function() {
+//     window.location.href = "masuk-login/login.html";
 // });
 
 
 // AOS
 AOS.init();
 
-document.addEventListener('DOMContentLoaded', function () {
+function initChat() {
     const chatOverlay = document.getElementById('chat-overlay');
     const chatToggle = document.getElementById('chat-toggle');
     const messageInput = document.getElementById('message-input');
@@ -40,11 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Event handler untuk menampilkan atau menyembunyikan chat overlay
     chatToggle.addEventListener('click', function () {
-        if (chatOverlay.style.right === '0px' || chatOverlay.style.right === '') {
-            chatOverlay.style.right = '-400px'; // Menyembunyikan chat
-        } else {
-            chatOverlay.style.right = '0px'; // Menampilkan chat
-        }
+        toggleChatOverlay();
     });
 
     sendButton.addEventListener('click', function () {
@@ -66,11 +69,83 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // JavaScript untuk menutup chat overlay saat tombol "X" diklik
     closeChatButton.addEventListener("click", function() {
-        chatOverlay.style.right = '-400px';
+        closeChatOverlay();
     });
 
     // Saat tombol ikon chat diklik, tampilkan chat overlay
     chatIcon.addEventListener("click", function() {
-        chatOverlay.style.right = '0px';
+        openChatOverlay();
     });
+
+    function toggleChatOverlay() {
+        if (chatOverlay.style.right === '0px' || chatOverlay.style.right === '') {
+            chatOverlay.style.right = '-400px'; // Menyembunyikan chat
+        } else {
+            chatOverlay.style.right = '0px'; // Menampilkan chat
+        }
+    }
+
+    function openChatOverlay() {
+        chatOverlay.style.right = '0px';
+    }
+
+    function closeChatOverlay() {
+        chatOverlay.style.right = '-400px';
+    }
+}
+
+// Panggil fungsi inisialisasi chat saat DOM selesai dimuat
+document.addEventListener('DOMContentLoaded', function () {
+    initChat();
 });
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     const chatOverlay = document.getElementById('chat-overlay');
+//     const chatToggle = document.getElementById('chat-toggle');
+//     const messageInput = document.getElementById('message-input');
+//     const sendButton = document.getElementById('send-button');
+//     const chatIcon = document.getElementById("chat-icon");
+//     const closeChatButton = document.getElementById("close-chat-overlay");
+//     const chatMessages = document.getElementById("chat-messages");
+
+//     // Event handler untuk menampilkan atau menyembunyikan chat overlay
+//     chatToggle.addEventListener('click', function () {
+//         if (chatOverlay.style.right === '0px' || chatOverlay.style.right === '') {
+//             chatOverlay.style.right = '-400px'; // Menyembunyikan chat
+//         } else {
+//             chatOverlay.style.right = '0px'; // Menampilkan chat
+//         }
+//     });
+
+//     sendButton.addEventListener('click', function () {
+//         const message = messageInput.value.trim();
+
+//         if (message !== '') {
+//             appendMessage('you', message);
+//             // Kirim pesan ke backend atau pemilik kos di sini
+//             messageInput.value = '';
+//         }
+//     });
+
+//     function appendMessage(sender, text) {
+//         const messageElement = document.createElement('div');
+//         messageElement.innerHTML = `<strong>${sender}:</strong> ${text}`;
+//         chatMessages.appendChild(messageElement);
+//         chatMessages.scrollTop = chatMessages.scrollHeight;
+//     }
+
+//     // JavaScript untuk menutup chat overlay saat tombol "X" diklik
+//     closeChatButton.addEventListener("click", function() {
+//         chatOverlay.style.right = '-400px';
+//     });
+
+//     // Saat tombol ikon chat diklik, tampilkan chat overlay
+//     chatIcon.addEventListener("click", function() {
+//         chatOverlay.style.right = '0px';
+//     });
+// });
